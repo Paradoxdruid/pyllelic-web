15d8e73c187de7d3e2f849442db0d39f858b2609
Paradoxdruid <bonham@gmail.com>
2022-12-16 09:13:46 -0700

    feat: working upload of files

--- pyllelic_web/app.py
+++ pyllelic_web/app.py
@@ -1,6 +1,6 @@
 """Web frontend for pyllelic bisulfite DNA analysis."""
 
-from typing import Dict, Union
+from typing import Dict, Optional, Union
 
 import dash
 import dash_bootstrap_components as dbc
@@ -16,20 +16,9 @@ app = dash.Dash(__name__, external_stylesheets=[THEME])
 app.title = "Pyllelic-Web"
 server = app.server
 
-# ---------------- Temporary Fixed Files for Testing ---------
+dash_uploader.configure_upload(app, "/tmp/test", use_upload_id=False)
 
-dash_uploader.configure_upload(app, "/tmp")
-
-CONFIG_OPTIONS: Dict[str, Union[str, int]] = {
-    "base_path": "./assets/",
-    "prom_file": "tert_genome.txt",
-    "prom_start": 1293200,
-    "prom_end": 1296000,
-    "chrom": "5",
-    "offset": 1293000,
-    "viz_backend": "plotly",
-    "test_dir": "test",
-}
+upload_config: Optional[Dict[str, Union[str, int]]] = None
 
 # ----------------- Main layout -----------------------------
 
@@ -41,7 +30,7 @@ app.layout = dbc.Container(
             dbc.Col(html.P("Pyllelic output of test data.")),
             class_name=PADDING,
         ),
-        dbc.Row(dash_uploader.Upload(id="dash-uploader")),
+        dbc.Row(dash_uploader.Upload(id="dash-uploader", max_files=4)),
         dbc.Row(SUBMIT_BUTTON, class_name=PADDING),
         dbc.Row(dbc.Col(html.Div(id="callback-output"))),
         dbc.Row(dbc.Col(html.Div(id="output-div"))),
@@ -61,27 +50,31 @@ def generate_graphs(n_clicks: int) -> Union[dbc.Container, html.Div]:
 
     else:
 
-        table, heatmap, reads_graph = run_pyllelic_and_graph(CONFIG_OPTIONS)
+        if upload_config:
+            table, heatmap, reads_graph = run_pyllelic_and_graph(upload_config)
 
-        return dbc.Container(
-            class_name="border border-primary rounded",
-            children=[
-                dbc.Row(
-                    dbc.Col(
-                        html.P(
-                            "Mean Methylation Data Table",
+            return dbc.Container(
+                class_name="border border-primary rounded",
+                children=[
+                    dbc.Row(
+                        dbc.Col(
+                            html.P(
+                                "Mean Methylation Data Table",
+                            ),
+                            width=7,
                         ),
-                        width=7,
+                        justify="center",
                     ),
-                    justify="center",
-                ),
-                dbc.Row(dbc.Col(table, width={"offset": 0, "size": 7})),
-                dbc.Row(
-                    dbc.Col(dcc.Graph(figure=heatmap), width={"offset": 1, "width": 6})
-                ),
-                dbc.Row(dbc.Col(dcc.Graph(figure=reads_graph))),
-            ],
-        )
+                    dbc.Row(dbc.Col(table, width={"offset": 0, "size": 7})),
+                    dbc.Row(
+                        dbc.Col(
+                            dcc.Graph(figure=heatmap), width={"offset": 1, "width": 6}
+                        )
+                    ),
+                    dbc.Row(dbc.Col(dcc.Graph(figure=reads_graph))),
+                ],
+            )
+        return dbc.Container(html.P("Invalid data"))
 
 
 @dash_uploader.callback(
@@ -89,4 +82,15 @@ def generate_graphs(n_clicks: int) -> Union[dbc.Container, html.Div]:
     id="dash-uploader",
 )  # type: ignore[misc]
 def callback_on_completion(status: dash_uploader.UploadStatus) -> html.Ul:
+    global upload_config
+    upload_config = {
+        "base_path": "/tmp/",
+        "prom_file": "/tmp/test/tert_genome.txt",
+        "prom_start": 1293200,
+        "prom_end": 1296000,
+        "chrom": "5",
+        "offset": 1293000,
+        "viz_backend": "plotly",
+        "test_dir": "test",
+    }
     return html.Ul([html.Li(str(a_file)) for a_file in status.uploaded_files])
